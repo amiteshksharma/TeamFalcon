17f0fb10701b1be31f766829e336104e9a993a47
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-08-30 12:58:21 -0700

    The server and client communicate

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello world');
+  console.log('HERE')
+  return res.send('Hello world');
 });
 
 module.exports = router;
