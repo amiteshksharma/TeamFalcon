db5e87c55b7988ffbc9845d030275717d38362b6
OmarElZaria <46201075+OmarElZaria@users.noreply.github.com>
2020-08-31 09:04:40 -0500

    added error checking and correct formating to personal_info page

--- client/src/Pages/Personal_info.js
+++ client/src/Pages/Personal_info.js
@@ -5,62 +5,116 @@ import { ROUTES } from "../routes";
 import "bootstrap/dist/css/bootstrap.css";
 import "../css/personal_info.css";
 
+const INITIAL_STATE = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  date: '',
+  location: '',
+  error: null,
+};
+
 class Personal_info extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = { zipcode: '' };
+    this.state = { ...INITIAL_STATE };
   }
 
-  handleChange = event => {
-    this.setState({ zipcode: event.target.value });
+  onSubmit = event => {
+    const { ...INITIAL_STATE } = this.state;
+
+    event.preventDefault();
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
   };
 
   render() {
+    const {
+      firstName,
+      lastName,
+      email,
+      date,
+      location,
+      error,
+    } = this.state;
+
+    const isInvalid =
+    firstName === '' || lastName === '' ||
+    email === '' || date === '' || location === '';
+
     return(
+
       <React.Fragment>
       
       <Container>
           <Card style={{padding: '20px'}}>
           <h1 class= "title">Let's get some information (?)</h1>
-          <Form>
+          <Form onSubmit={this.onSubmit}>
           <Form.Row>
             <Form.Group as={Col} controlId="formGridFirstName">
               <Form.Label class="font-weight-bold">First Name</Form.Label>
-              <Form.Control type="first name" />
+              <Form.Control 
+                  name="firstName"
+                  value={firstName}
+                  onChange={this.handleChange}
+                  type="text" 
+              />
             </Form.Group>
 
           <Form.Group as={Col} controlId="formGridLastName">
               <Form.Label class="font-weight-bold">Last Name</Form.Label>
-              <Form.Control type="last name"/>
+              <Form.Control 
+                  name="lastName"
+                  value={lastName}
+                  onChange={this.handleChange}
+                  type="text"
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="formGridEmailAddress">
             <Form.Label class="font-weight-bold">Email Address</Form.Label>
-            <Form.Control/>
+            <Form.Control
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                type="email"
+            />
           </Form.Group>
 
           <Form.Group controlId="formGridDateOfBirth">
             <Form.Label class="font-weight-bold">Date of Birth</Form.Label>
-            <Form.Control placeholder= "MM/DD/YYYY" />
+            <Form.Control 
+                name="date"
+                value={date}
+                onChange={this.handleChange}
+                type="date" 
+                placeholder= "YYYY-MM-DD" 
+            />
           </Form.Group>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridZipCode">
               <Form.Label class="font-weight-bold">Location</Form.Label>
               <Form.Control 
-                type = "text"
-                name = "zipcode"
-                value = {this.state.username}
-                onChange = {this.handleChange}
-                placeholder= "Zip Code" />
+                name="location"
+                value={location}
+                onChange={this.handleChange}
+                type="text"
+                placeholder="Zip Code" 
+                maxLength={6}
+              />
           </Form.Group>
           </Form.Row>
           
           <div class="text-center">
             <Link to={ROUTES.SYMPTOMS}>
-              <Button class="text-center" 
+              <Button 
+                      disabled={isInvalid}
+                      class="text-center" 
                       onClick={() => {
                         console.log("The button has been clicked");
                       }}
@@ -68,6 +122,7 @@ class Personal_info extends React.Component{
                       type="submit">
                 Next: Select Symptoms
               </Button>
+              {error && <p>{error.message}</p>}
             </Link>
           </div>
         </Form>
@@ -78,4 +133,7 @@ class Personal_info extends React.Component{
     ); 
   }
 }
+
+
+
 export default Personal_info
