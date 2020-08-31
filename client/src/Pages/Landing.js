83aeb8bbbcaeb26d853dfe328445277cb70711d6
allisonchen506 <allisonchen506@gmail.com>
2020-08-30 21:14:03 -0700

    finished front end for landing page

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Container, Row, Col, Button} from 'react-bootstrap';
+import { ROUTES } from '../routes';
+import {Link} from "react-router-dom";
 class Landing extends React.Component {
   render() {
-    return <h1>Landing</h1>
+    return (
+      <div className="text-center">
+        <h1 class="title">Welcome to this Website</h1>
+        <p align="center">This website is dedicated to help people that are
+        in need of a doctor's appointment</p>
+        <Link to={ROUTES.INFO}>
+          <Button 
+                onClick={()=>{
+                  console.log("Button has been clicked");
+                }}
+                variant="primary"
+                type="submit">Get Started</Button>
+        </Link>
+      </div>
+
+  )
   }
 }
 export default Landing;
