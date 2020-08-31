55de6537cb26a5d01abac04dafad76a4bd4e2a84
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-08-30 17:04:14 -0700

    api call is done

--- client/src/Pages/Symptoms.js
+++ client/src/Pages/Symptoms.js
@@ -1,5 +1,12 @@
 import React from 'react'
+
 class Symptoms extends React.Component {
+  componentDidMount() {
+    // fetch('/symptoms')
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+  }
+
   render() {
     return <h1>Symptoms</h1>
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+require('dotenv').config()
+const fetch = require('node-fetch')
 
 /* GET home page. */
-router.get('/hello', function(req, res, next) {
-  console.log('HERE')
-  return res.send('Hello world');
+router.get('/symptoms', function(req, res, next) {
+  fetch(`https://healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFtaXRlc2guc2hhcm1hLjIwMDFAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI1MDUyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMC0wOC0zMCIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU5ODgzODI1NywibmJmIjoxNTk4ODMxMDU3fQ.shZZo8OVRGVlr6UsqLdn0Xy3gEKfXEU7zuve799pP98&format=json&language=en-gb`)
+    .then(response => response.json())
+    .then(data => {
+      return res.send(data);
+    })
+    .catch(error => {
+      console.log(error)
+  })
 });
 
 module.exports = router;
