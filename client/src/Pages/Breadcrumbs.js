4386a3a843e6ce563b4a1fa4661d605ea4652d81
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-09-01 12:54:14 -0700

    small changes to help with deployment

--- client/src/Pages/Breadcrumbs.js
+++ client/src/Pages/Breadcrumbs.js
@@ -8,7 +8,7 @@ const Example = (props) => {
       <Breadcrumb>
         <BreadcrumbItem><Link to='/' className="Home">Team falcon</Link></BreadcrumbItem>
         <BreadcrumbItem><Link to='/personal_info' className="Info">Info</Link></BreadcrumbItem>
-        <BreadcrumbItem ><Link to='/symptoms' className="Symptoms">Symptoms</Link></BreadcrumbItem>
+        <BreadcrumbItem ><Link to='/bodysymptoms' className="Symptoms">Symptoms</Link></BreadcrumbItem>
         <BreadcrumbItem ><Link to='/symptomlist' className="SymptomList">More Symptoms</Link></BreadcrumbItem>
         <BreadcrumbItem ><Link to='/preliminarydiagnosis' className="Priliminarydiagnosis">Preliminary Diagnosis</Link></BreadcrumbItem>
         <BreadcrumbItem ><Link to='/clinics_near_me' className="Clinics">Clinics</Link></BreadcrumbItem>
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,7 @@
 export const ROUTES = {
     LANDING: "/",
     INFO: "/personal_info",
-    SYMPTOMS: "/symptoms",
+    SYMPTOMS: "/bodysymptoms",
     SYMPTOM_LIST: "/symptomlist",
     PRELIMINARYDIAGNOSIS: "/preliminarydiagnosis",
     CLINICS: "/clinics_near_me",
