1d310b8aba7c6a8ba5886cfc33ca9ae82375b9dd
OmarElZaria <46201075+OmarElZaria@users.noreply.github.com>
2020-09-01 22:37:37 -0500

    removed navbar links beacause we have breadcrumbs

--- client/src/Pages/Navigation.js
+++ client/src/Pages/Navigation.js
@@ -2,22 +2,16 @@ import React from "react";
 import { Navbar, Nav } from "../../node_modules/react-bootstrap";
 import { ROUTES } from "../routes";
 import "../bootstrap-scoped.scss";
+import Image from 'react-bootstrap/Image'
+import logo from "../logo.png"
 
 const Navigation = () => (
   <div className="bootstrap-scoped">
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href={ROUTES.LANDING} className="brand-name">
-        TEAM FALCON
+      <Navbar.Brand href={ROUTES.LANDING} className="brand-name" >
+        <Image src={logo}/>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href={ROUTES.INFO}>INFO</Nav.Link>
-          <Nav.Link href={ROUTES.SYMPTOMS}>SYMPTOMS</Nav.Link>
-          <Nav.Link href={ROUTES.PreliminaryDiagnosis}>PRELIMINARY DIAGNOSIS</Nav.Link>
-          <Nav.Link href={ROUTES.CLINICS}>CLINICS</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
     </Navbar>
   </div>
   );
