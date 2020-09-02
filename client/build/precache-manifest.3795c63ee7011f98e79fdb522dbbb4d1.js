5d801e44d8bfc0d69dc135f889d13ac6cc206793
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-09-02 09:28:14 -0700

    new build folder

---
+++ client/build/precache-manifest.3795c63ee7011f98e79fdb522dbbb4d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74a0f89a7ac996613862e695c687910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d41194122e0f3076ae9",
+    "url": "/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "1d13814d80319c874543",
+    "url": "/static/css/main.4a989bc8.chunk.css"
+  },
+  {
+    "revision": "4d41194122e0f3076ae9",
+    "url": "/static/js/2.7848829b.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.7848829b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d13814d80319c874543",
+    "url": "/static/js/main.8c7754a1.chunk.js"
+  },
+  {
+    "revision": "9fcbd0e01832af0c482d",
+    "url": "/static/js/runtime-main.66eefc99.js"
+  },
+  {
+    "revision": "dc3808c658b3d1341646b04f14d50293",
+    "url": "/static/media/aaron-lee-_qWnSEsA1Ls-unsplash.dc3808c6.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bdbd9893b2ab3a4e346bc54ac7b8f3f.js"
+  "/precache-manifest.3795c63ee7011f98e79fdb522dbbb4d1.js"
 );
 
 self.addEventListener('message', (event) => {
