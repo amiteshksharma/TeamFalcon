72538644918768e3fde542fc1a80f8be2ee92828
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-09-08 15:32:25 -0700

    small change

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,45 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const admin = require('firebase-admin');
 require('dotenv').config()
 const fetch = require('node-fetch')
 
-console.log(admin.firestore());
-
 /* GET home page. */
 router.get('/symptoms', function(req, res, next) {
-  // const string = 'https://sandbox-healthservice.priaid.ch'
-  const string = 'https://healthservice.priaid.ch'
-  fetch(`${string}/symptoms?token=${process.env.SECRET_KEY}&format=json&language=en-gb`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      return res.send(data);
-    })
-    .catch(error => {
-      console.log(error)
-  })
-});
-
-router.post('/diagnosis', function(req, res, next) {
-  const symptoms = req.body.Symptoms;
-  const jsonify = JSON.stringify(symptoms)
-  const gender = req.body.Gender;
-  const year = req.body.Age ? req.body.Age : 2000;
-
-  console.log(req.body);
-
-  fetch(`https://healthservice.priaid.ch/diagnosis?symptoms=${jsonify}&gender=${gender}&year_of_birth=${year}&token=${process.env.DIAGNOSIS_KEY}&format=json&language=en-gb`, {
-    method: 'GET'
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      return res.send(data);
-    })
-    .catch(error => {
-      console.log(error)
-  })
+  // Dummy Route
 });
 
 module.exports = router;
