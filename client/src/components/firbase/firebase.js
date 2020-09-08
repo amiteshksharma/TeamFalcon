8c881088e5519d56ecb01a0a16ae112eaf3910f6
OmarElZaria <46201075+OmarElZaria@users.noreply.github.com>
2020-09-08 17:40:52 -0500

    fixed compile time errors with firebase

--- client/src/components/firbase/firebase.js
+++ client/src/components/firbase/firebase.js
@@ -1,7 +1,17 @@
-import firebaseConfig from "../../../../server/app";
 import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD93hKn54YxT1sAE5qKA3DZMyvAz0P6tKQ",
+  authDomain: "falcon-project-f2431.firebaseapp.com",
+  databaseURL: "https://falcon-project-f2431.firebaseio.com",
+  projectId: "falcon-project-f2431",
+  storageBucket: "falcon-project-f2431.appspot.com",
+  messagingSenderId: "555496500915",
+  appId: "1:555496500915:web:831bc1d32d318fa8504ac6",
+  measurementId: "G-E492JJPFVW"
+}
 
 class Firebase {
     constructor() {
@@ -33,4 +43,4 @@ class Firebase {
     }
   }
   
-  export const firebase = new Firebase();
+  export default Firebase;
